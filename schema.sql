39cc33d31cb44a17f1c02edc52c51bbad37ba144
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-09-20 12:45:24 +0100

    Finish the first project

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+	id int,
+	name varchar(255), 
+	date_of_birth date, 
+	escape_attempts int, 
+	neutered bit, 
+	weight_kg decimal)
+;
